d05cd787df4d195904ab6491c517be657b3a0d46
Petros <petroskitazos@gmail.com>
2023-10-26 13:34:02 +0100

    added forked matchingproblems package, and requirements file

--- run_generator.py
+++
@@ -1,5 +0,0 @@
-from matchingproblems import generator
-import sys
-
-if __name__ == "__main__":
-    generator = generator.Generator(sys.argv[1:])
---
+++ temp/run_solver.py
@@ -0,0 +1,39 @@
+from matchingproblems import solver
+
+args = ['-na', '3', '-maxsize', '1', '-gen', '2', '-lsb', '3']
+
+data = {
+    "students": [
+        [1, 7],
+        [1, 2, 3, 4, 5, 6],
+        [2, 1, 4],
+        [2],
+        [1, 2, 3, 4],
+        [2, 3, 4, 5, 6],
+        [5, 3, 8]
+    ],
+    "projects": [
+        [0, 2, 1],
+        [0, 1, 1],
+        [0, 1, 1],
+        [0, 1, 2],
+        [0, 1, 2],
+        [0, 1, 2],
+        [0, 1, 3],
+        [0, 1, 3],
+    ],
+    "lecturers": [
+        [0, 2, 3],
+        [0, 2, 2],
+        [0, 2, 2]
+    ],
+}
+
+
+if __name__ == "__main__":
+    solver = solver.Solver(args, data)
+    solver.solve(msg=False, timeLimit=None, threads=None, write=False)
+    # print(solver.get_debug())
+    # print(solver.get_results_long())
+    # print(solver.get_results_short())
+    print(solver.get_results())
