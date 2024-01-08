e36ab96573623d2d337539aadcaaa0128ebb8545
Petros <petroskitazos@gmail.com>
2024-01-08 20:29:24 +0000

    refactored classes to match the new shape of the matching data received from the allocation app

--- src/server_data.py
+++ src/server_data.py
@@ -1,4 +1,4 @@
-from src.request_data import RequestData
+from src.request_data import ProjectData, RequestData, StudentData, SupervisorData
 
 
 class ServerData:
@@ -8,41 +8,43 @@ class ServerData:
         self.p_to_int = None
         self.p_to_str = None
         self.s_to_str = None
-        self.lecturers = self.process_lecturers(data.lecturers)
+        self.lecturers = self.process_lecturers(data.supervisors)
         self.projects = self.process_projects(data.projects)
         self.students = self.process_students(data.students)
 
-    def process_lecturers(self, lecturer_arr: list[tuple[str, int, int, int]]):
+    def process_lecturers(self, lecturer_arr: list[SupervisorData]):
         to_int = {}
         lecturer_data = []
         for idx, lecturer in enumerate(lecturer_arr, 1):
-            (lecturer_id, lb, t, ub) = lecturer
-            to_int[lecturer_id] = idx
-            lecturer_data.append([lb, t, ub])
+            to_int[lecturer.id] = idx
+            data = [lecturer.lowerBound, lecturer.target, lecturer.upperBound]
+            lecturer_data.append(data)
 
         self.l_to_int = to_int
         return lecturer_data
 
-    def process_projects(self, project_arr: list[tuple[int, int, str]]):
+    def process_projects(self, project_arr: list[ProjectData]):
         to_int = {}
         to_str = {}
         project_data = []
         for idx, project in enumerate(project_arr, 1):
-            (project_id, lb, ub, lecturer_id) = project
-            to_int[project_id] = idx
-            to_str[idx] = project_id
-            project_data.append([lb, ub, self.l_to_int[lecturer_id]])
+            to_int[project.id] = idx
+            to_str[idx] = project.id
+            data = [project.lowerBound, project.upperBound,
+                    self.l_to_int[project.supervisorId]]
+            project_data.append(data)
 
         self.p_to_int = to_int
         self.p_to_str = to_str
         return project_data
 
-    def process_students(self, student_arr: list[list[str]]):
+    def process_students(self, student_arr: list[StudentData]):
         student_data = []
         to_str = {}
-        for idx, (student_id, *student) in enumerate(student_arr):
-            student_data.append(list(map(lambda x: self.p_to_int[x], student)))
-            to_str[idx] = student_id
+        for idx, student in enumerate(student_arr):
+            data = [self.p_to_int[x] for x in student.preferences]
+            student_data.append(data)
+            to_str[idx] = student.id
 
         self.s_to_str = to_str
         return student_data
