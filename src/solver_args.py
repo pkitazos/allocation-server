4e9c65c0bf2761f3d85eecffd8bb05a7d7b85d5a
Petros <93052465+pkitazos@users.noreply.github.com>
2024-07-20 20:10:38 +0100

    Created algorithm args enum

---
+++ src/solver_args.py
@@ -0,0 +1,4 @@
+class Args:
+    GENEROUS = ["-na", "3", "-maxsize", "1", "-gen", "2", "-lsb", "3"]
+    GREEDY = ["-na", "3", "-maxsize", "1", "-gre", "2", "-lsb", "3"]
+    MINCOST = ["-na", "3", "-maxsize", "1", "-mincost", "2", "-lsb", "3"]
